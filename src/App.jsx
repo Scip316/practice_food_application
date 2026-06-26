c7d1501d261a3868357d8dbe2a60bdc63f9e5b34
Scip316 <darreldominiclim316@gmail.com>
2026-06-26 17:13:53 +0800

    added next winner logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
 import { useState } from 'react';
 
 export default function Board() {
+  const [X_isNext,setX_isNext] = useState(true);
   const [squares, setSquares] = useState(Array(0).fill(null));
+
+  function handleClick(i){
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+    const nextSquares = squares.slice()
+
+    if (X_isNext){
+      nextSquares[i] = "X"
+    } else {
+      nextSquares[i] = "O"
+    }
+
+    setSquares(nextSquares)
+    setX_isNext(!X_isNext)
+    console.log("clicked " + squares);
+  }
+
   return (
   <>
   <div className="board-row">
-    <Square value={squares[0]}/>
-    <Square value={squares[1]}/>
-    <Square value={squares[2]}/>
+    <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+    <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+    <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
   </div>
     <div className="board-row">
-    <Square value={squares[3]}/>
-    <Square value={squares[4]}/>
-    <Square value={squares[5]}/>
+    <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+    <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+    <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
   </div>
   <div className="board-row">
-    <Square value={squares[6]}/>
-    <Square value={squares[7]}/>
-    <Square value={squares[8]}/>
+    <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+    <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+    <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
   </div>
   </>
   );
+    return null
 }
 
-function Square({value}) {
+function Square({value, onSquareClick}) {
   // const [value, setValue] = useState(null)
   function handleClick(){
     console.log("clicked");
+    console.log({value});
     // setValue("X")
   }
-  return <button className="square" onClick={handleClick}>{value}</button>;
+  return <button className="square" onClick={onSquareClick}>{value}</button>;
 
 }
 
